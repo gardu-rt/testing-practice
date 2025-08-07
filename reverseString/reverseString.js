c22aac15b9d955212c899fc0efede5a39b221fb4
kapindo <kapindo@gmail.com>
2025-08-07 09:24:55 +0700

    feat: reverse string

---
+++ reverseString/reverseString.js
@@ -0,0 +1,3 @@
+export function reverseString(s) {
+  return [...s].reverse().join("");
+}
---
+++ reverseString/reverseString.test.js
@@ -0,0 +1,5 @@
+import { reverseString } from "./reverseString.js";
+
+test("hello to be olleh", () => {
+  expect(reverseString("hello")).toBe("olleh");
+});
